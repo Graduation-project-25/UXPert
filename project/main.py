968598be8858ccca3483a04fa9237c5a20c9703d
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2025-01-25 15:08:55 +0200

    main

--- project/main.py
+++ project/main.py
@@ -40,48 +40,48 @@ def main():
     dataset_types = ['EGFE', 'Rico']
     elements, normalized_data = extract_ui_elements(json_file_path, dataset_types[0])
 
-    # # Aggregate by 'type'
-    # aggregated_elements = aggregate_ui_elements(normalized_data)
-    # #print("Aggregated Elements:\n", aggregated_elements)
-    # print("***************************************************************\n")
+    # Aggregate by 'type'
+    aggregated_elements = aggregate_ui_elements(normalized_data)
+    #print("Aggregated Elements:\n", aggregated_elements)
+    print("***************************************************************\n")
 
-    # # Scatter plot of UI elements
-    # #scatter_plot_ui_elements(normalized_data)
+    # Scatter plot of UI elements
+    #scatter_plot_ui_elements(normalized_data)
     
-    # #splitting dataset
-    # X_train,X_test = split_dataset(normalized_data)
-    # print("Training Data:\n", X_train)
-    # print("Testing Data:\n", X_test)
-    # print("***************************************************************\n")
+    #splitting dataset
+    X_train,X_test = split_dataset(normalized_data)
+    print("Training Data:\n", X_train)
+    print("Testing Data:\n", X_test)
+    print("***************************************************************\n")
 
-    # # DBSCAN Clustering
-    # X_train,DBSCAN_dataset, clusters = dbscan_cluster(X_train)
-    # handle_outliers(X_train)
-    # evaluate_clustering(DBSCAN_dataset)
-    # analyze_clusters(DBSCAN_dataset)
-    # clustering_visulaization_by_size(DBSCAN_dataset,clusters)
-    # clustering_visualization_by_position(DBSCAN_dataset,clusters)
-    # # visualize_alignment_consistency(DBSCAN_dataset)
+    # DBSCAN Clustering
+    X_train,DBSCAN_dataset, clusters = dbscan_cluster(X_train)
+    handle_outliers(X_train)
+    evaluate_clustering(DBSCAN_dataset)
+    analyze_clusters(DBSCAN_dataset)
+    clustering_visulaization_by_size(DBSCAN_dataset,clusters)
+    clustering_visualization_by_position(DBSCAN_dataset,clusters)
+    # visualize_alignment_consistency(DBSCAN_dataset)
     
-    # visualize_color_consistency(DBSCAN_dataset)
-    # visualize_size_proportionality(DBSCAN_dataset)
-    # # visualize_ui_elements(image_folder, json_folder, output_folder, limit=50)
+    visualize_color_consistency(DBSCAN_dataset)
+    visualize_size_proportionality(DBSCAN_dataset)
+    # visualize_ui_elements(image_folder, json_folder, output_folder, limit=50)
 
     
-    # print("#######################################################################################")
+    print("#######################################################################################")
 
-    # # Test data clustering
-    # print("Clustering test data...")
-    # clustered_test_data, _, _ = dbscan_cluster(X_test)
+    # Test data clustering
+    print("Clustering test data...")
+    clustered_test_data, _, _ = dbscan_cluster(X_test)
 
-    # print("Evaluating test data clustering...")
-    # evaluate_clustering(clustered_test_data)
+    print("Evaluating test data clustering...")
+    evaluate_clustering(clustered_test_data)
 
-    # # Assign test clusters
-    # new_x_test = assign_test_clusters(DBSCAN_dataset, X_test, clusters)
+    # Assign test clusters
+    new_x_test = assign_test_clusters(DBSCAN_dataset, X_test, clusters)
 
-    # # Evaluate test results
-    # evaluate_test_clusters(new_x_test, DBSCAN_dataset)
+    # Evaluate test results
+    evaluate_test_clusters(new_x_test, DBSCAN_dataset)
     
 if __name__ == "__main__":
     main()
