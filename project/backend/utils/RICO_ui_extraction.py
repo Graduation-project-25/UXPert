e4f3245a6d7796d033c69eb433e1df1e3ffbbbc8
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2025-01-25 17:28:57 +0200

    rico normalization

--- project/backend/utils/RICO_ui_extraction.py
+++ project/backend/utils/RICO_ui_extraction.py
@@ -1,5 +1,8 @@
 import json
 
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
 
 def extract_rico_ui_elements(json_file_path):
     """Extract UI elements from Rico dataset."""
@@ -31,4 +34,29 @@ def extract_rico_ui_elements(json_file_path):
     if "activity" in data and "root" in data["activity"]:
         traverse(data["activity"]["root"])
 
-    return elements
+
+    # Normalize json data into a flat table
+    df = pd.json_normalize(elements)
+
+    # Normalize into scaled data 
+    normalized_data = normalize_ui_elements(elements, df)
+    # print("Normalized, Scaled Data:\n", normalized_data)
+    # print("***************************************************************\n")    
+    
+    return elements, normalized_data
+
+def normalize_ui_elements(elements, df):
+    """Normalize the UI elements' positional and dimensional data."""
+    # Instantiate a scaler for the numerical columns: width, height, x, and y
+    scaler = MinMaxScaler()
+
+    # Select only the numeric columns for scaling
+    numeric_cols = ['width', 'height', 'position.x', 'position.y']
+    df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+    # df = pd.get_dummies(df, columns=['type'], prefix='type')
+    df['clickable'] = df['clickable'].astype(int)
+    df['enabled'] = df['enabled'].astype(int)
+
+
+    # Return the normalized and scaled data as a DataFrame
+    return df
