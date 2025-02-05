dbcb86d3a3daf3aca3da20b3db71c8d0906750fa
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2025-02-05 22:35:59 +0200

    project.

--- project/main.py
+++ project/main.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 
 from components.Heuristics_Component.heuristics_evaluation.minimalist_evaluation import MinimalistEvaluation
+from components.Heuristics_Component.heuristic_rules.Consistency_using_clusters import ClusteringConsistency
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from components.Clustering_Component.EGFE_clustering import EGFE_Clustering
