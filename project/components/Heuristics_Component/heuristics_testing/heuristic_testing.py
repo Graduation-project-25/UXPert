015bd8d474d8148b36e83e710eefa55cb0be91c1
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2025-02-06 17:20:21 +0200

    testing

---
+++ project/components/Heuristics_Component/heuristics_testing/heuristic_testing.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+class HeuristicTestingInterface(ABC):
+    
+    @abstractmethod
+    def evaluate_rule():
+        pass
---
+++ project/components/Heuristics_Component/heuristics_testing/minimalist_testing.py
@@ -0,0 +1,8 @@
+
+
+from components.Heuristics_Component.heuristics_testing.heuristic_testing import HeuristicTestingInterface
+from components.Heuristics_Component.heuristic_rules.heuristic_factory import HeuristicFactory
+
+class MinimalistEvaluation(HeuristicTestingInterface): 
+    def evaluate_rule():
+        print("Evaluate minimalistic rule")
