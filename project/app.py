e654d15490dcc669ce445b3c07eaec9f4c286da6
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2025-02-07 02:26:00 +0200

    save designs in folder

--- project/app.py
+++ project/app.py
@@ -4,15 +4,26 @@ import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from components.Heuristics_Component.heuristic_rules.consistency import Consistency
+from components.Heuristics_Component.heuristic_rules.heuristic_factory import HeuristicFactory
+from components.Heuristics_Component.heuristics_evaluation.minimalist_evaluation import MinimalistEvaluation
 
 # Initialize Flask
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
 
 # Define output folder
-OUTPUT_FOLDER = "project/data/figma_features"
-os.makedirs(OUTPUT_FOLDER, exist_ok=True)  # Ensure the folder exists
-OUTPUT_FILE = os.path.join(OUTPUT_FOLDER, "output.json")  # Save in folder
+data_folder = "project/data/figma_features"
+output_folder = data_folder + "/extracted"
+evaluation_folder = data_folder + "/evaluation"
+os.makedirs(data_folder, exist_ok=True)  # Ensure the folder exists
+os.makedirs(output_folder, exist_ok=True)  # Ensure the folder exists
+os.makedirs(evaluation_folder, exist_ok=True)  # Ensure the folder exists
+
+def get_new_filename():
+    """Generate a unique filename based on existing files in the extracted folder."""
+    existing_files = [f for f in os.listdir(output_folder) if f.endswith(".json")]
+    count = len(existing_files)  # Count current files and use it for a new filename
+    return os.path.join(output_folder, f"design_{count + 1}.json")
 
 @app.route('/process', methods=['POST', 'OPTIONS'])
 def process_elements():
@@ -28,7 +39,6 @@ def process_elements():
     design_name = data.get("design_name", "Untitled Design")
     frame_info = data.get("frame", {})
     elements = data.get('elements', [])
-
     if not elements:
         return jsonify({"error": "No elements found"}), 400
 
@@ -39,8 +49,11 @@ def process_elements():
 
     try:
         # Evaluate consistency
-        consistency_evaluator = Consistency()
+        consistency_evaluator = HeuristicFactory.check_rule("consistency")
         consistency_results = consistency_evaluator.evaluate_rule(elements_df)
+
+        minimalist_evaluator = MinimalistEvaluation()
+        white_space_ratio_evaluator = minimalist_evaluator.evaluate_white_space_ratio(output_folder, evaluation_folder)
 
         print(f"Consistency evaluation results: {consistency_results}")
 
@@ -55,14 +68,19 @@ def process_elements():
 
        
         output_data = {
-            "user_name": user_name,
-            "design_name": design_name,
-            "frame": frame_info,  
+            # "user_name": user_name,
+            # "design_name": design_name,
+            "screen_size": frame_info,  
             "elements": elements,
             "consistency_results": feedback 
         }
 
-        with open(OUTPUT_FILE, "w", encoding="utf-8") as json_file:
+        output_file = get_new_filename() # Save in folder
+        # count+=1
+        # print(count)
+        print("*********************************************************************************************")
+
+        with open(output_file, "w", encoding="utf-8") as json_file:
             json.dump(output_data, json_file, indent=4, ensure_ascii=False)
 
         return jsonify({
